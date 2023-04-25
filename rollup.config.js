10ed3151478529c5a6f0bea0504d794e02f8afeb
Michael Overmeyer <michael.overmeyer@shopify.com>
2023-04-25 15:13:09 -0400

    Add some build system stuff

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import { argv } from "yargs";
+
+const format = argv.format || argv.f || "iife";
+const compress = argv.compact;
+
+const babelOptions = {
+  exclude: "node_modules/**",
+  presets: [["@babel/preset-env", { modules: false }]],
+  babelrc: false,
+};
+
+const output = [
+  {
+    file: `dist/umd/i18nextShopify${compress ? ".min" : ""}.js`,
+    format: "umd",
+    name: "i18nextShopify",
+  },
+  {
+    file: `dist/amd/i18nextShopify${compress ? ".min" : ""}.js`,
+    format: "amd",
+    name: "i18nextShopify",
+  },
+  {
+    file: `dist/iife/i18nextShopify${compress ? ".min" : ""}.js`,
+    format: "iife",
+    name: "i18nextShopify",
+  },
+];
+
+export default {
+  input: "src/index.js",
+  plugins: [
+    babel(babelOptions),
+    nodeResolve({ mainField: ["jsnext:main"] }),
+  ].concat(compress ? terser() : []),
+  output,
+};
