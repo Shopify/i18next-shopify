bc8e8fa0987e12b8ffc63c7112878f6b881c8043
Michael Overmeyer <michael.overmeyer@shopify.com>
2023-05-02 14:12:04 -0400

    Add support for react-i18next

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,29 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
     node: true,
   },
   extends: [
     "plugin:@shopify/jest",
     "plugin:@shopify/esnext",
+    "plugin:testing-library/react",
+    "plugin:jest-dom/recommended",
+    "plugin:react/recommended",
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      impliedStrict: true,
+    },
   },
   rules: {
     "@babel/object-curly-spacing": "off",
--- test/shopify.spec.js
+++ test/shopify.spec.js
@@ -1,5 +1,10 @@
+import React from 'react';
 import areIntlLocalesSupported from "intl-locales-supported";
 import i18next from "i18next";
+import { cleanup, render } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+import { initReactI18next, Trans, useTranslation } from 'react-i18next';
+import "@testing-library/jest-dom";
 
 import ShopifyFormat from "../src";
 
@@ -270,6 +275,320 @@ describe("shopify format", () => {
           ordinal: 2,
         }),
       ).toBe("This is my 2st car");
+    });
+  });
+
+  describe("with react-i18next (t)", () => {
+    beforeEach(() => {
+      i18next
+        .use(initReactI18next)
+        .use(ShopifyFormat)
+        .init({
+          lng: "en",
+          resources: {
+            en: {
+              string: "Hello world!",
+              string_with_interpolation:
+              "Hello {{casual_name}}! Today is {{date}}.",
+              string_with_repeated_interpolation:
+              "Hello {{casual_name}}! Hello {{casual_name}}!",
+              cardinal_pluralization: {
+                0: "I have no cars.",
+                one: "I have {{count}} car.",
+                other: "I have {{count}} cars.",
+              },
+              cardinal_pluralization_with_missing_keys: {
+                other: "I have {{count}} cars.",
+              },
+              ordinal_pluralization: {
+                ordinal: {
+                  one: "This is my {{ordinal}}st car",
+                  two: "This is my {{ordinal}}nd car",
+                  few: "This is my {{ordinal}}rd car",
+                  other: "This is my {{ordinal}}th car",
+                },
+              },
+            },
+          },
+        });
+    });
+
+    it("handles basic lookups", () => {
+      const { result } = renderHook(() => useTranslation('translation'));
+      const { t } = result.current;
+
+      expect(t("string")).toBe("Hello world!");
+      expect(
+        t("string_with_interpolation", {
+          casual_name: "Joe",
+          date: "Monday",
+        }),
+      ).toBe("Hello Joe! Today is Monday.");
+      expect(
+        t("string_with_repeated_interpolation", {
+          casual_name: "Joe",
+        }),
+      ).toBe("Hello Joe! Hello Joe!");
+    });
+
+    it("does not fail when given excess values", () => {
+      const { result } = renderHook(() => useTranslation('translation'));
+      const { t } = result.current;
+
+      expect(
+        t("string_with_interpolation", {
+          casual_name: "Joe",
+          date: "Monday",
+          unnecessary: "This value is not mentioned by any interpolation",
+        }),
+      ).toBe("Hello Joe! Today is Monday.");
+    });
+
+    it("handles cardinal pluralization lookups", () => {
+      const { result } = renderHook(() => useTranslation('translation'));
+      const { t } = result.current;
+
+      expect(t("cardinal_pluralization", { count: 1 })).toBe(
+        "I have 1 car.",
+      );
+      expect(t("cardinal_pluralization", { count: 2 })).toBe(
+        "I have 2 cars.",
+      );
+    });
+
+    it("falls back to the `other` key if the proper cardinal pluralization key is missing", () => {
+      const { result } = renderHook(() => useTranslation('translation'));
+      const { t } = result.current;
+
+      expect(
+        // A count of 1 in `en` should use the `one` key, but it's missing.
+        // It should fall back to the `other` key.
+        t("cardinal_pluralization_with_missing_keys", { count: 1 }),
+      ).toBe("I have 1 cars.");
+    });
+
+    it("allows explicit lookup of cardinal pluralization subkeys", () => {
+      const { result } = renderHook(() => useTranslation('translation'));
+      const { t } = result.current;
+
+      expect(t("cardinal_pluralization.one", { count: 0 })).toBe(
+        "I have 0 car.",
+      );
+    });
+
+    it("prefers explicit 0 keys in cardinal pluralization lookups", () => {
+      const { result } = renderHook(() => useTranslation('translation'));
+      const { t } = result.current;
+
+      expect(t("cardinal_pluralization", { count: 0 })).toBe(
+        "I have no cars.",
+      );
+    });
+
+    it("handles ordinal pluralization lookups (using ordinal: true)", () => {
+      const { result } = renderHook(() => useTranslation('translation'));
+      const { t } = result.current;
+
+      expect(
+        t("ordinal_pluralization", { count: 1, ordinal: true }),
+      ).toBe("This is my 1st car");
+      expect(
+        t("ordinal_pluralization", { count: 2, ordinal: true }),
+      ).toBe("This is my 2nd car");
+      expect(
+        t("ordinal_pluralization", { count: 3, ordinal: true }),
+      ).toBe("This is my 3rd car");
+      expect(
+        t("ordinal_pluralization", { count: 4, ordinal: true }),
+      ).toBe("This is my 4th car");
+    });
+
+    it("handles ordinal pluralization lookups (using ordinal: <number>)", () => {
+      const { result } = renderHook(() => useTranslation('translation'));
+      const { t } = result.current;
+
+      expect(t("ordinal_pluralization", { ordinal: 1 })).toBe(
+        "This is my 1st car",
+      );
+      expect(t("ordinal_pluralization", { ordinal: 2 })).toBe(
+        "This is my 2nd car",
+      );
+      expect(t("ordinal_pluralization", { ordinal: 3 })).toBe(
+        "This is my 3rd car",
+      );
+      expect(t("ordinal_pluralization", { ordinal: 4 })).toBe(
+        "This is my 4th car",
+      );
+    });
+
+    it("allows explicit lookup of ordinal pluralization subkeys", () => {
+      const { result } = renderHook(() => useTranslation('translation'));
+      const { t } = result.current;
+
+      expect(
+        t("ordinal_pluralization.ordinal.one", {
+          count: 2,
+          ordinal: true,
+        }),
+      ).toBe("This is my 2st car");
+      expect(
+        t("ordinal_pluralization.ordinal.one", {
+          ordinal: 2,
+        }),
+      ).toBe("This is my 2st car");
+    });
+  });
+
+  describe("with react-i18next (Trans)", () => {
+    beforeEach(() => {
+      cleanup();
+      i18next
+        .use(initReactI18next)
+        .use(ShopifyFormat)
+        .init({
+          lng: "en",
+          resources: {
+            en: {
+              string: "Hello world!",
+              string_with_interpolation:
+              "Hello {{casual_name}}! Today is {{date}}.",
+              string_with_repeated_interpolation:
+              "Hello {{casual_name}}! Hello {{casual_name}}!",
+              cardinal_pluralization: {
+                0: "I have no cars.",
+                one: "I have {{count}} car.",
+                other: "I have {{count}} cars.",
+              },
+              cardinal_pluralization_with_missing_keys: {
+                other: "I have {{count}} cars.",
+              },
+              ordinal_pluralization: {
+                ordinal: {
+                  one: "This is my {{ordinal}}st car",
+                  two: "This is my {{ordinal}}nd car",
+                  few: "This is my {{ordinal}}rd car",
+                  other: "This is my {{ordinal}}th car",
+                },
+              },
+            },
+          },
+        });
+    });
+
+    it("handles basic lookups", () => {
+      const TestComponent = () => (
+        <>
+          <Trans i18nKey="string"/>
+          <Trans i18nKey="string_with_interpolation" values={{casual_name: "Joe", date: "Monday"}}/>
+          <Trans i18nKey="string_with_repeated_interpolation" values={{casual_name: "Joe"}}/>
+        </>
+      );
+      const { container } = render(<TestComponent />);
+      expect(container.childNodes[0]).toHaveTextContent("Hello world!");
+      expect(container.childNodes[1]).toHaveTextContent("Hello Joe! Today is Monday.");
+      expect(container.childNodes[2]).toHaveTextContent("Hello Joe! Hello Joe!");
+    });
+
+    it("does not fail when given excess values", () => {
+      const TestComponent = () => (
+        <>
+          <Trans i18nKey="string_with_interpolation" values={{
+            casual_name: "Joe",
+            date: "Monday",
+            unnecessary: "This value is not mentioned by any interpolation",
+          }}/>
+        </>
+      );
+      const { container } = render(<TestComponent />);
+
+      expect(container.childNodes[0]).toHaveTextContent("Hello Joe! Today is Monday.");
+    });
+
+    it("handles cardinal pluralization lookups", () => {
+      const TestComponent = () => (
+        <>
+          <Trans i18nKey="cardinal_pluralization" count={1}/>
+          <Trans i18nKey="cardinal_pluralization" count={2}/>
+        </>
+      );
+      const { container } = render(<TestComponent />);
+      expect(container.childNodes[0]).toHaveTextContent("I have 1 car.");
+      expect(container.childNodes[1]).toHaveTextContent("I have 2 cars.");
+    });
+
+    it("falls back to the `other` key if the proper cardinal pluralization key is missing", () => {
+      const TestComponent = () => (
+        <>
+          <Trans i18nKey="cardinal_pluralization_with_missing_keys" count={1}/>
+        </>
+      );
+      const { container } = render(<TestComponent />);
+      // A count of 1 in `en` should use the `one` key, but it's missing.
+      expect(container.childNodes[0]).toHaveTextContent("I have 1 cars.");
+    });
+
+    it("allows explicit lookup of cardinal pluralization subkeys", () => {
+      const TestComponent = () => (
+        <>
+          <Trans i18nKey="cardinal_pluralization.one" count={0}/>
+        </>
+      );
+      const { container } = render(<TestComponent />);
+      expect(container.childNodes[0]).toHaveTextContent("I have 0 car.");
+    });
+
+    it("prefers explicit 0 keys in cardinal pluralization lookups", () => {
+      const TestComponent = () => (
+        <>
+          <Trans i18nKey="cardinal_pluralization" count={0}/>
+        </>
+      );
+      const { container } = render(<TestComponent />);
+      expect(container.childNodes[0]).toHaveTextContent("I have no cars.");
+    });
+
+    it("handles ordinal pluralization lookups (using ordinal: true)", () => {
+      const TestComponent = () => (
+        <>
+          <Trans i18nKey="ordinal_pluralization" values={{count: 1, ordinal: true}}/>
+          <Trans i18nKey="ordinal_pluralization" values={{count: 2, ordinal: true}}/>
+          <Trans i18nKey="ordinal_pluralization" values={{count: 3, ordinal: true}}/>
+          <Trans i18nKey="ordinal_pluralization" values={{count: 4, ordinal: true}}/>
+        </>
+      );
+      const { container } = render(<TestComponent />);
+      expect(container.childNodes[0]).toHaveTextContent("This is my 1st car");
+      expect(container.childNodes[1]).toHaveTextContent("This is my 2nd car");
+      expect(container.childNodes[2]).toHaveTextContent("This is my 3rd car");
+      expect(container.childNodes[3]).toHaveTextContent("This is my 4th car");
+    });
+
+    it("handles ordinal pluralization lookups (using ordinal: <number>)", () => {
+      const TestComponent = () => (
+        <>
+          <Trans i18nKey="ordinal_pluralization" values={{ordinal: 1}}/>
+          <Trans i18nKey="ordinal_pluralization" values={{ordinal: 2}}/>
+          <Trans i18nKey="ordinal_pluralization" values={{ordinal: 3}}/>
+          <Trans i18nKey="ordinal_pluralization" values={{ordinal: 4}}/>
+        </>
+      );
+      const { container } = render(<TestComponent />);
+      expect(container.childNodes[0]).toHaveTextContent("This is my 1st car");
+      expect(container.childNodes[1]).toHaveTextContent("This is my 2nd car");
+      expect(container.childNodes[2]).toHaveTextContent("This is my 3rd car");
+      expect(container.childNodes[3]).toHaveTextContent("This is my 4th car");
+    });
+
+    it("allows explicit lookup of ordinal pluralization subkeys", () => {
+      const TestComponent = () => (
+        <>
+          <Trans i18nKey="ordinal_pluralization.ordinal.one" count={2} values={{ordinal: true}}/>
+          <Trans i18nKey="ordinal_pluralization.ordinal.one" values={{ordinal: 2}}/>
+        </>
+      );
+      const { container } = render(<TestComponent />);
+      expect(container.childNodes[0]).toHaveTextContent("This is my 2st car");
+      expect(container.childNodes[1]).toHaveTextContent("This is my 2st car");
     });
   });
 });
